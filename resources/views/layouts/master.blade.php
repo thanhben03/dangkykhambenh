437ff3c88bcc5ce0e720846759294a2ebd1b3746
= <=>
2024-11-01 13:31:49 +0700

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,48 +61,36 @@
                     <i class="fa fa-fw fa-bars"></i>
                 </button>
 
-                <!-- App Search-->
-                <form class="app-search d-none d-lg-block">
-                    <div class="position-relative">
-                        <input type="text" class="form-control" placeholder="Search...">
-                        <button class="btn btn-primary" type="button"><i class="bx bx-search-alt align-middle"></i></button>
-                    </div>
-                </form>
+            
             </div>
 
             <div class="d-flex align-items-center">
 
                 <div class="dropdown d-none d-lg-inline-block ms-1 mx-3">
-                    <div class="d-flex">
+                    <div class="d-flex align-items-center">
                             <div id="currentDateTime"></div>
                             <strong>
-                                <span class="mx-2">Khoa {{ Auth::user()->department->department_name }}</span>
+                                <span style="font-size: 17px" class="mx-2">Khoa {{ Auth::user()->department->department_name }}</span>
                                 
                             </strong>                        
                     </div>
                 </div>
 
-                <div class="dropdown d-inline-block">
+                <div class="d-flex align-items-center">
                     <button type="button" class="btn header-item bg-light-subtle border-start border-end" id="page-header-user-dropdown"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-1.jpg"
                              alt="Header Avatar">
                         <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ Auth::user()->name }}</span>
-                        <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                        
                     </button>
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <!-- item-->
-                        <a class="dropdown-item" href="apps-contacts-profile.html"><i class="mdi mdi mdi-face-man font-size-16 align-middle me-1"></i> Profile</a>
-                        <a class="dropdown-item" href="auth-lock-screen.html"><i class="mdi mdi-lock font-size-16 align-middle me-1"></i> Lock Screen</a>
-                        <div class="dropdown-divider"></div>
-                        <form action="{{route('logout')}}" method="POST">
-                            @csrf
-                            <button class="dropdown-item" type="submit">
-                                <i class="mdi mdi-logout font-size-16 align-middle me-1"></i> 
-                                Logout
-                            </button>
-                        </form>
-                    </div>
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <button class="dropdown-item" type="submit">
+                            <i class="mdi mdi-logout font-size-16 align-middle me-1"></i> 
+                            Logout
+                        </button>
+                    </form>
                 </div>
 
             </div>
