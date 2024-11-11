f49719aa644c54222f5716e52790241dfbc958c2
= <=>
2024-11-11 23:30:59 +0700

    f

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,7 +5,7 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '5409e448d421808b3abf',
+    key: 'fd867c95fcf23f864c2e',
     cluster: 'ap1',
     forceTLS: true,
 });
