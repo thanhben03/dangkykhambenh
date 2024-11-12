7d770af60bf8756b0883194c69d7c205dd5c6a1c
= <=>
2024-11-13 00:59:29 +0700

    f

--- resources/views/admin/quan-ly-bac-si.blade.php
+++ resources/views/admin/quan-ly-bac-si.blade.php
@@ -44,12 +44,7 @@ Quản Lý Bác Sĩ
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                             <h4 class="mb-sm-0 font-size-18">Quản lý bác sĩ</h4>
-                            <div class="page-title-right">
-                                <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                    <li class="breadcrumb-item active">Quản lý bác sĩ</li>
-                                </ol>
-                            </div>
+                            
 
                         </div>
                         <button onclick="showModalCreateDoctor()" class="btn btn-primary mb-3">Tạo tài khoản</button>
