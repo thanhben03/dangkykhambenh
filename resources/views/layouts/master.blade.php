e80cca1b5dbd9de0061a8aaa37ad20154986c79a
= <=>
2024-11-14 17:00:48 +0700

    f

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -136,6 +136,13 @@
                                     <span data-key="t-medical-history">Lịch Sử Khám Bệnh</span>
                                 </a>
                             </li>
+                            <li>
+                                <a href="/pending-screen">
+                                    <i class="bx bx-fullscreen"></i>
+
+                                    <span data-key="t-manage-doctor">Màn hình chờ</span>
+                                </a>
+                            </li>
                         @else
                             <li>
                                 <a href="/manage-patient">
@@ -151,13 +158,7 @@
                                     <span data-key="t-manage-doctor">Quản lý bác sĩ</span>
                                 </a>
                             </li>
-                            <li>
-                                <a href="/pending-screen">
-                                    <i class="bx bx-fullscreen"></i>
-
-                                    <span data-key="t-manage-doctor">Màn hình chờ</span>
-                                </a>
-                            </li>
+                            
                         @endif
 
 
