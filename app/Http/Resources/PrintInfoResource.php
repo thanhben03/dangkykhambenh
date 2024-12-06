6a128660eed3c6e5a648aef23cafbe65834b0860
= <=>
2024-12-06 22:00:41 +0700

    f

--- app/Http/Resources/PrintInfoResource.php
+++ app/Http/Resources/PrintInfoResource.php
@@ -19,8 +19,8 @@ class PrintInfoResource extends JsonResource
             'cccd' => $this->patient->nic,
             'bod' => $this->patient->bod,
             'sex' => $this->patient->sex == 'Male' ? 'Nam' : 'Nữ',
-            'trieu_chung' => $this->trieu_chung,
-            'chuan_doan' => $this->chuan_doan,
+            'symptom' => $this->symptom,
+            'diagnosis' => $this->diagnosis,
             'address' => $this->patient->address,
             'history_medicine' => implode(', ', $this->medicines->map(function ($item) {
                 return $item->medicine_name.'/'.$item->qty.'('.$item->use.')';
