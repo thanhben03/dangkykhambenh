7995e1901668dddd7588a53a1049b37abf118210
thanhben03 <nben19733@gmail.com>
2024-12-15 14:40:46 +0700

    f

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -24,16 +26,33 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        // $request->authenticate();
 
-        $request->session()->regenerate();
+        // $request->session()->regenerate();
 
-        if (Auth::user()->user_type == 'admin') {
+        // if (Auth::user()->user_type == 'admin') {
+        //     return redirect()->intended(route('showPatient', absolute: false));
+        // }
+
+        // return redirect()->intended(route('dashboard', absolute: false));
+
+        $data = $request->all();
+        $user = User::query()->where('email', '=', $data['email'])->first();
+        if (!$user) {
+            return redirect()->back()->withErrors(['email' => 'Không tìm thấy tài khoản này.']);
+        }
+
+        if (!Hash::check($data['password'], $user->password)) {
+            return back()->withErrors(['password' => 'Mật khẩu không đúng.']);
+        }
+
+        Auth::login($user);
+
+        if ($user->user_type == 'admin') {
             return redirect()->intended(route('showPatient', absolute: false));
         }
 
         return redirect()->intended(route('dashboard', absolute: false));
-
     }
 
     /**
