abc474222d1be1b782788ea25dc8c2418a26c065
thanhben03 <nben19733@gmail.com>
2024-12-16 20:22:57 +0700

    f

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,11 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <!-- @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+            @if (Route::has('password.request'))
+                <a onclick="alertForget()" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="#">
                     {{ __('Quên mật khẩu ?') }}
                 </a>
-            @endif -->
+            @endif
 
             <x-primary-button class="ms-3">
                 {{ __('Đăng Nhập') }}
@@ -45,3 +45,9 @@
         </div>
     </form>
 </x-guest-layout>
+
+<script>
+    function alertForget() {
+        alert('Vui lòng liên hệ hotline: 123456 để được cấp lại mật khẩu !')
+    }
+</script>
